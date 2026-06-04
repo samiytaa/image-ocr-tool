e845a37dea68036d0f8ccc8f2b20de118c63b12d
samiytaa <samiytaa@users.noreply.github.com>
2026-06-05 05:49:39 +0800

    Configure GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/image-ocr-tool/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
