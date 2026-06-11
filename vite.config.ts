e18c40c54ea85b53e50af547fd4958092a2e8bc9
samiytaa <samiytaa@users.noreply.github.com>
2026-06-11 18:00:53 +0800

    fix: set correct pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/',
+    base: process.env.NODE_ENV === 'production' ? '/image-ocr-tool/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
